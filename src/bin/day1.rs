464762f268847247782935d2758f92e30b8506f2
Jørgen P. Tjernø <jorgen@tjer.no>
2021-12-05 13:11:04 -0800

    Day 1: Solve part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -7,19 +7,31 @@ fn main() {
     let file = File::open("day1.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let mut depths = reader
+    let depths = reader
         .lines()
-        .filter_map(|line| line.map(|s| s.parse::<u32>().unwrap()).ok());
+        .filter_map(|line| line.map(|s| s.parse::<u32>().unwrap()).ok())
+        .collect::<Vec<_>>();
 
-    let initial_value = (0, depths.next().unwrap());
-
-    let (increases, _) = depths.fold(initial_value, |(count, previous), value| {
+    let count_increases = |(count, previous), value| {
         if value > previous {
             (count + 1, value)
         } else {
             (count, value)
         }
-    });
+    };
 
+    let initial_value = (0, &depths[0]);
+    let (increases, _) = depths.iter().skip(1).fold(initial_value, count_increases);
     println!("Depth increases: {}", increases);
+
+    let sliding_windows = (0..depths.len() - 2)
+        .map(|index| depths[index] + depths[index + 1] + depths[index + 2])
+        .collect::<Vec<_>>();
+
+    let initial_value = (0, &sliding_windows[0]);
+    let (increases, _) = sliding_windows
+        .iter()
+        .skip(1)
+        .fold(initial_value, count_increases);
+    println!("Depth increases (sliding window): {}", increases);
 }
