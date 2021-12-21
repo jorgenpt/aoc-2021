66da512e505f62aeefa9a0f0d49ee58ca503344e
Jørgen P. Tjernø <jorgen@tjer.no>
2021-12-20 20:54:46 -0800

    Day 10: Solve part 1

---
+++ src/day10.rs
@@ -0,0 +1,96 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[derive(PartialEq)]
+pub enum ChunkAction {
+    Open,
+    Close,
+}
+
+#[derive(PartialEq, Clone, Copy)]
+pub enum ChunkType {
+    Soff,
+    Square,
+    Squiggly,
+    Angle,
+}
+
+impl ChunkType {
+    fn get_score(&self) -> usize {
+        match self {
+            ChunkType::Soff => 3,
+            ChunkType::Square => 57,
+            ChunkType::Squiggly => 1197,
+            ChunkType::Angle => 25137,
+        }
+    }
+}
+
+#[aoc_generator(day10)]
+pub fn generator(input: &str) -> Vec<Vec<(ChunkAction, ChunkType)>> {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .filter_map(|c| match c {
+                    '(' => Some((ChunkAction::Open, ChunkType::Soff)),
+                    ')' => Some((ChunkAction::Close, ChunkType::Soff)),
+                    '[' => Some((ChunkAction::Open, ChunkType::Square)),
+                    ']' => Some((ChunkAction::Close, ChunkType::Square)),
+                    '{' => Some((ChunkAction::Open, ChunkType::Squiggly)),
+                    '}' => Some((ChunkAction::Close, ChunkType::Squiggly)),
+                    '<' => Some((ChunkAction::Open, ChunkType::Angle)),
+                    '>' => Some((ChunkAction::Close, ChunkType::Angle)),
+                    _ => None,
+                })
+                .collect()
+        })
+        .collect()
+}
+
+#[aoc(day10, part1)]
+pub fn part1(lines: &Vec<Vec<(ChunkAction, ChunkType)>>) -> usize {
+    lines
+        .into_iter()
+        .map(|line| {
+            let mut chunk_stack = Vec::<ChunkType>::new();
+            for (chunk_action, chunk_type) in line {
+                if *chunk_action == ChunkAction::Open {
+                    chunk_stack.push(*chunk_type);
+                } else {
+                    if let Some(last_chunk) = chunk_stack.last() {
+                        if last_chunk != chunk_type {
+                            return chunk_type.get_score();
+                        } else {
+                            chunk_stack.pop();
+                        }
+                    } else {
+                        return chunk_type.get_score();
+                    }
+                }
+            }
+
+            0
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "[({(<(())[]>[[{[]{<()<>>\n\
+                         [(()[<>])]({[<{<<[]>>(\n\
+                         {([(<{}[<>[]}>{[]{[(<()>\n\
+                         (((({<>}<{<{<>}{[]{[]{}\n\
+                         [[<[([]))<([[{}[[()]]]\n\
+                         [{[{({}]{}}([{[{{{}}([]\n\
+                         {<[[]]>}<{[{[{[]{()[[[]\n\
+                         [<(<(<(<{}))><([]([]()\n\
+                         <{([([[(<>()){}]>(<<{{\n\
+                         <{([{{}}[<[[[<>{}]]]>[]]";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(&generator(INPUT)), 26397);
+    }
+}
