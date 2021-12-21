7a1955a42dc02dc9da243ad36f91d3db1d48a101
Jørgen P. Tjernø <jorgen@tjer.no>
2021-12-20 21:09:19 -0800

    Day 10: Solve part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,7 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
+use itertools::Itertools;
+
 #[derive(PartialEq)]
 pub enum ChunkAction {
     Open,
@@ -21,6 +23,14 @@ impl ChunkType {
             ChunkType::Square => 57,
             ChunkType::Squiggly => 1197,
             ChunkType::Angle => 25137,
+        }
+    }
+    fn get_remainder_score(&self) -> usize {
+        match self {
+            ChunkType::Soff => 1,
+            ChunkType::Square => 2,
+            ChunkType::Squiggly => 3,
+            ChunkType::Angle => 4,
         }
     }
 }
@@ -74,6 +84,42 @@ pub fn part1(lines: &Vec<Vec<(ChunkAction, ChunkType)>>) -> usize {
         .sum()
 }
 
+#[aoc(day10, part2)]
+pub fn part2(lines: &Vec<Vec<(ChunkAction, ChunkType)>>) -> usize {
+    let scores = lines
+        .into_iter()
+        .filter_map(|line| {
+            let mut chunk_stack = Vec::<ChunkType>::new();
+            for (chunk_action, chunk_type) in line {
+                if *chunk_action == ChunkAction::Open {
+                    chunk_stack.push(*chunk_type);
+                } else {
+                    if let Some(last_chunk) = chunk_stack.last() {
+                        if last_chunk != chunk_type {
+                            return None;
+                        } else {
+                            chunk_stack.pop();
+                        }
+                    } else {
+                        return None;
+                    }
+                }
+            }
+
+            Some(chunk_stack)
+        })
+        .map(|remaining_stack| {
+            remaining_stack
+                .into_iter()
+                .rev()
+                .map(|t| t.get_remainder_score())
+                .fold(0, |accum, score| 5 * accum + score)
+        })
+        .sorted()
+        .collect_vec();
+    scores[scores.len() / 2]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -92,5 +138,10 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1(&generator(INPUT)), 26397);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&generator(INPUT)), 288957);
     }
 }
